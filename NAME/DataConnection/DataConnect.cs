cf4bf9a01de7ea7ad554790d1f3916ed76d7e85c
PixelSyntax55 <syedmuhammadkhubaibgillani@gmail.com>
2026-06-03 00:07:23 +0500

    git error

---
+++ NAME/DataConnection/DataConnect.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using CrudLevel1.NAME.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CrudLevel1.DataConnection 
+{
+    
+public class DataConnect : DbContext
+{
+
+        public DataConnect(DbContextOptions<DataConnect> options) : base(options) { }
+
+
+        public DbSet <Person> Persons { get; set; }
+
+ }
+ }
---
+++ NAME/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace CrudLevel1.NAME.Models
+{
+    public class Person
+    {
+
+        int Id { get; set; }
+
+        string Name { get; set; } = string.Empty;
+
+        int Age { get; set; }
+      
+        int PhoneNumber { get; set; }
+
+        string Email { get; set; } = string.Empty;
+
+        string Gender { get; set; } = string.Empty;
+
+        string City { get; set; } = string.Empty;
+
+
+    }
+}
